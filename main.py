7b14ef38c5a921015d8cb245c64c00d8976d2d4d
Gefry123 <gefryquero27@gmail.com>
2026-05-20 17:26:31 -0400

    cleaned main.py

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from src import data_fetcher as df
 from src import signals as sig
 from src import news_fetcher as nf
 from src import reporter as rep
+import sys
 
-data_frame_data = df.fetch_price_data("NVDA")
 
-print(rep.generate_report("NVDA", sig.flag_signals(
-    data_frame_data), nf.fetch_news("NVIDIA stock")))
+script_name = sys.argv[0]
+
+if len(sys.argv) > 1:
+    ticker = sys.argv[1]
+else:
+    "NVDA"
+
+data_frame_data = df.fetch_price_data(ticker)
+
+print(rep.generate_report(ticker, sig.flag_signals(
+    data_frame_data), nf.fetch_news(f"{ticker} stock")))
