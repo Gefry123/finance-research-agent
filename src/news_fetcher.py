eb89374d9e69a52be2c9501ab7401e08491433df
Gefry123 <gefryquero27@gmail.com>
2026-06-23 17:54:30 -0400

    Added scrape_article function to news_fetcher.py

--- src/news_fetcher.py
+++ src/news_fetcher.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import os
 from newsapi import NewsApiClient
+from bs4 import BeautifulSoup
+import requests
 
 load_dotenv()
 news_api_key = os.getenv("NEWS_API_KEY")
@@ -16,4 +18,7 @@ def format_headlines(news: list):
     return [{"title": a["title"], "description": a["description"], "url": a["url"]} for a in news]
 
 
-print(type(fetch_news("NVIDIA STOCK")))
+def scrape_article(url: str):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    return soup.get_text()
